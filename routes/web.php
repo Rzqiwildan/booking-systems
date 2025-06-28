e4c17bbcd0d579dbaeb15f9accc7df747f345727
Rizqi wildan <126966398+Rzqiwildan@users.noreply.github.com>
2025-06-28 12:03:21 +0700

    test lagi

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use App\Http\Controllers\BookingController;
 use Illuminate\Support\Facades\Route;
-use Filament\Facades\Filament;
 
 Route::get('/', [BookingController::class, 'index'])->name('booking');
 Route::post('/booking', [BookingController::class, 'submitForm'])->name('form.submit');
-Route::middleware(['auth'])->group(function () {
-    Filament::routes(); // Memanggil rute Filament
-});
